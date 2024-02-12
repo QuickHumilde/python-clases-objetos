43c18d07ec1c4ce807a0c2acc444d37cb2ab87f3
Diego Fernández Domínguez <148773876+Quick-Humilde@users.noreply.github.com>
2024-02-12 14:47:22 +0100

    Update reto1.py

--- reto1.py
+++ reto1.py
@@ -1,8 +1,5 @@
 # 1.-Crea un programa calculadora que tenga un menú para seleccionar las diferentes 
 # operaciones de suma, resta, multiplicación y división y se ejecute hasta que el usuario decida parar.
-# 2.-Crea una nueva versión del programa anterior en el que su modo de ejecución sea 
-# que la persona que ejecuta el programa pasa como argumentos la operación a 
-# realizar y los operandos. Ejemplo: "python calculadora.py resta 3 2" Debe devolver "3 -2 = 1"
 
 if __name__=="__main__":
     def menu():
